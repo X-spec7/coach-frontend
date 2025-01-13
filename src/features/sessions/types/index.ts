e80d00a4eb7a91a9f7e00b2a3af955970ec26307
X-spec7 <mercury.spec77@gmail.com>
2025-01-14 05:50:06 +0900

    feat: define session type

---
+++ src/features/sessions/types/index.ts
@@ -0,0 +1,19 @@
+interface ISession {
+  // basic info
+  title: string
+  startDate: string
+  duration: number // in minutes
+  // coach info
+  coachId: number
+  coachFullname: string
+  // session desc
+  goal: string
+  level: string
+  description: string
+  // booking info
+  totalParticipantNumber: number
+  currentParticipantNumber: number
+  price: number
+  // equip
+  equipments: string[]
+}
