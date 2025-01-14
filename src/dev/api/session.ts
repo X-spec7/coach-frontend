d9808f97649c6fd9007f5d0a951a8f423e77993b
X-spec7 <mercury.spec77@gmail.com>
2025-01-14 03:22:56 +0100

    feat: create get total session count mock api

--- src/dev/api/session.ts
+++ src/dev/api/session.ts
@@ -1,6 +1,11 @@
-import { GetSessionsRequestDTO, GetSessionsResponseDTO } from "@/features/sessions/types"
 import { mockApi } from "./api"
 import { sessionsDummyData } from "../dummy-data/sessions"
+import {
+  GetSessionsRequestDTO,
+  GetSessionsResponseDTO,
+  GetTotalSessionCountRequestDTO,
+  GetTotalSessionCountResponseDTO,
+} from "@/features/sessions/types"
 
 export const getSessionsMockApi = async (request: GetSessionsRequestDTO): Promise<GetSessionsResponseDTO> => {
   const { limit, offset, goal, booked, query } = request
@@ -29,5 +34,37 @@ export const getSessionsMockApi = async (request: GetSessionsRequestDTO): Promis
       sessions: paginatedSessions
     },
     500 // Simulate a delay of 500ms
+  )
+}
+
+export const getTotalSessionCountMockApi = async (
+  request: GetTotalSessionCountRequestDTO
+): Promise<GetTotalSessionCountResponseDTO> => {
+  const { limit, offset, goal, booked, query } = request
+
+  let filteredSessions = sessionsDummyData
+
+  if (goal) {
+    filteredSessions = filteredSessions.filter(
+      (session) => session.goal === goal
+    )
+  }
+
+  if (query) {
+    filteredSessions = filteredSessions.filter(
+      (session) =>
+        session.title.toLowerCase().includes(query.toLowerCase()) ||
+        session.description.toLowerCase().includes(query.toLowerCase())
+    )
+  }
+
+  const paginatedSessions = filteredSessions.slice(offset, offset + limit)
+
+  return mockApi<GetTotalSessionCountResponseDTO>(
+    {
+      message: 'Session Total Count fetched successfully',
+      totalSessionCount: paginatedSessions.length
+    },
+    500
   )
 }
--- src/features/sessions/service/client.service.ts
+++ src/features/sessions/service/client.service.ts
@@ -1,9 +1,22 @@
-import { getSessionsMockApi } from '@/dev/api/session'
-import { GetSessionsRequestDTO, GetSessionsResponseDTO } from '../types'
+import { getSessionsMockApi, getTotalSessionCountMockApi } from '@/dev/api/session'
+import {
+  GetSessionsRequestDTO,
+  GetSessionsResponseDTO,
+  GetTotalSessionCountRequestDTO,
+  GetTotalSessionCountResponseDTO
+} from '../types'
 
 class ClientSessionService {
-  getSessions = async (request: GetSessionsRequestDTO): Promise<GetSessionsResponseDTO> => {
+  getSessions = async (
+    request: GetSessionsRequestDTO
+  ): Promise<GetSessionsResponseDTO> => {
     return getSessionsMockApi(request)
+  }
+
+  getTotalSessionCount = async (
+    request: GetTotalSessionCountRequestDTO
+  ): Promise<GetTotalSessionCountResponseDTO> => {
+    return getTotalSessionCountMockApi(request)
   }
 }
 
