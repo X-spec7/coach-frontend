b6697c146e5e3d8919b5beaf207e9da833e81139
X-spec7 <mercury.spec77@gmail.com>
2025-01-22 20:06:41 +0900

    feat: create meeting service

--- src/features/messages/service/index.ts
+++ src/features/messages/service/index.ts
@@ -1,2 +1,3 @@
 export { default as contactService } from './contact.service'
 export { default as messageService } from './message.service'
+export { default as meetingService } from './meeting.service'
