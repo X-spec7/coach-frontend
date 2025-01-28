39ec7e35864cdb7d3d053b2eb6bf6c27788d408a
X-spec7 <mercury.spec77@gmail.com>
2025-01-29 04:28:14 +0900

    fix: useRoleGuard hook in client component

--- src/features/statistics/pages/index.tsx
+++ src/features/statistics/pages/index.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import ContentHeader from './content-header'
 import StatisticsContent from './content'
 
@@ -8,10 +10,10 @@ const StatisticsPage = () => {
   useRoleGuard('Client')
   
   return (
-      <div className='flex flex-col gap-4 p-4 rounded-4xl bg-white'>
-        <ContentHeader workoutTime={{hour: 12, minute: 35}} totalWorkouts={14} />
-        <StatisticsContent />
-      </div>
+    <div className='flex flex-col gap-4 p-4 rounded-4xl bg-white'>
+      <ContentHeader workoutTime={{hour: 12, minute: 35}} totalWorkouts={14} />
+      <StatisticsContent />
+    </div>
   )
 }
 
--- src/shared/hooks/useRoleGuard.ts
+++ src/shared/hooks/useRoleGuard.ts
@@ -1,5 +1,3 @@
-'use client'
-
 import { useRouter } from 'next/navigation'
 import { useAuth } from '../provider'
 import { IUserRoleGuard } from '../types'
